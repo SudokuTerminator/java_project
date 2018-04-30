bfd0734d36a66f981b0a1cfb8a3eb3a5be137932
SudokuTerminator <SudokuTerminator@github.com>
2018-04-30 13:17:25 +0800

    stl

---
+++ stl/Equation.java
@@ -0,0 +1,8 @@
+package stl;
+class Equation{
+	private static float fcofact
+	private static float fdeterminat(float[][] A){
+		if(A.length != A[0].length)
+			return (Double) null;
+		else if(A.length == 2)
+			return 
